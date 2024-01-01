1cde715920063de93885047781f351067a0d46a8
Jason Motylinski <jason@motylinski.com>
2024-01-01 09:11:22 -0500

    feature: Shuffle images on load

--- static/js/element.js
+++ static/js/element.js
@@ -220,7 +220,7 @@ export default class KenBurnsCarousel extends HTMLElement {
                 break;
             case "images" /* Images */:
                 this.images = newVal
-                    ? newVal.split(' ').filter(url => url)
+                    ? shuffle(newVal.split(' ').filter(url => url))
                     : [];
                 break;
             case "slide-duration" /* SlideDuration */:
@@ -314,3 +314,22 @@ function arraysEqual(arr1, arr2) {
     }
     return true;
 }
+
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex > 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
+  
